bb8943e863f49a82d2eae262f7753ef7fa71ac58
Marcin-esz <msz.marcin.szewczyk@gmail.com>
2018-05-31 22:51:08 +0200

    projekt w c

---
+++ c/CubeRemace/main.c
@@ -0,0 +1,26 @@
+/*
+ * main.c    ATmega328P    F_CPU = 16000000 Hz
+ *
+ * Created on: 31.05.2018
+ *     Author: Marcin Szewczyk
+*/
+// do³¹czanie systemowych plików nag³ówkowych
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/pgmspace.h>
+#include "mk_build.h"
+
+// miejsce na do³¹czanie w³asnych plików
+
+
+// g³ówna funkcja programu - main()
+int main( void ) {
+
+// sekcja inicjalizacji peryferiów
+
+// g³ówna pêtla programu
+    while ( 1 ) {
+
+    }
+
+}
