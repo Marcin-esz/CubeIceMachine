3d8e3a2542d3f825d147643de44de63602184e39
Marcin-esz <msz.marcin.szewczyk@gmail.com>
2018-05-31 22:38:52 +0200

    edit code

--- arduino/CubeRemake/CubeRemake.ino
+++ arduino/CubeRemake/CubeRemake.ino
@@ -1,10 +1,55 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Adafruit_GFX.h>
+#include <Adafruit_SPITFT.h>
+#include <Adafruit_SPITFT_Macros.h>
+#include <gfxfont.h>
+
+#include <Adafruit_SSD1306.h>
+
+
+
+
+// Zmienne
+int mVperAmp = 185; // dla moduĹ‚u 20A - 100, dla moduĹ‚u 30A - 66
+int Wartosc_analogowa= 0; // 
+int ACSoffset = 2472;
+float  napiecie = 0;
+double natezenie= 0;
+double srednie_natezenie =0;
+int srednia = 50;
+
+void setup() 
+{
+
+
+   pinMode(3, OUTPUT);
+   digitalWrite(3,HIGH);
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
 
+void loop() {
+
+  srednie_natezenie = 0;
+  for(int i=0;i<=srednia;i++)
+  {
+     Wartosc_analogowa = analogRead(A0);
+     napiecie = (Wartosc_analogowa / 1024.0) * 5000; // napiÄ™cie w mV
+     natezenie = ((napiecie - ACSoffset) / mVperAmp)*1000; // natÄ™ĹĽenie w mA
+
+     srednie_natezenie =  srednie_natezenie+ natezenie;
+
+     delay(1);
+  }
+  
+  srednie_natezenie= srednie_natezenie/srednia;
+  Serial.println(srednie_natezenie);
+
+
+  if (srednie_natezenie >800)
+  {
+      digitalWrite(3,LOW);
+  }
+    
+  
 }
 
